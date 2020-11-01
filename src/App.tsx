cc3aef1205d95c4d4bf745a901c37829ee8be56f
Deo <deo.uviray@gmail.com>
2020-11-01 19:37:42 +0800

    Add Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Card from './components/card/Card'
+
 function App() {
   return (
     <div className="App">
-
+    <Card imgSrc="https://upload.wikimedia.org/wikipedia/en/a/aa/Hulk_%28circa_2019%29.png" title="Hulk" />
+    <Card imgSrc="http://www.pngall.com/wp-content/uploads/2016/05/Spider-Man-Free-Download-PNG.png" title="Spiderman" />
     </div>
   );
 }
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './card.css'
+
+interface Props {
+    imgSrc: string,
+    title: string
+}
+
+const Card: React.FC<Props> = ({imgSrc, title}) => {
+    return (
+        <div className="Card">
+            <div className="Image">
+                <img src={imgSrc} alt="Card image" />
+            </div>
+            <div className="Details">
+                <p>{title}</p>
+                <div className="Buttons">
+                    <button className="Edit">Edit</button>
+                    <button className="Remove">Remove</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
